9265d504bcc50dde23ce10912afa1653387175c5
Chris <chris_pens@hotmail.co.uk>
2022-09-21 20:24:18 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Autocomplete, TextField } from "@mui/material";
+import "./App.css";
+
+const results = {
+  results: [
+    {
+      country: "GB",
+      city: "Aberdeen",
+      count: 621734,
+      locations: 3,
+      firstUpdated: "2016-02-27T20:00:00+00:00",
+      lastUpdated: "2022-09-21T05:00:00+00:00",
+      parameters: ["no2", "o3", "pm10", "pm25"],
+    },
+    {
+      country: "GB",
+      city: "Adur",
+      count: 29061,
+      locations: 1,
+      firstUpdated: "2021-04-29T00:00:00+00:00",
+      lastUpdated: "2022-01-31T14:00:00+00:00",
+      parameters: ["no2", "pm10", "pm25"],
+    },
+    {
+      country: "GB",
+      city: "Armagh",
+      count: 234378,
+      locations: 1,
+      firstUpdated: "2016-02-27T21:00:00+00:00",
+      lastUpdated: "2022-09-21T05:00:00+00:00",
+      parameters: ["no2", "pm10"],
+    },
+    {
+      country: "GB",
+      city: "Aston Hill",
+      count: 239035,
+      locations: 1,
+      firstUpdated: "2016-02-27T21:00:00+00:00",
+      lastUpdated: "2022-09-21T05:00:00+00:00",
+      parameters: ["no2", "o3"],
+    },
+    {
+      country: "GB",
+      city: "Auchencorth",
+      count: 365517,
+      locations: 1,
+      firstUpdated: "2016-02-27T21:00:00+00:00",
+      lastUpdated: "2022-09-21T05:00:00+00:00",
+      parameters: ["o3", "pm10", "pm25"],
+    },
+    {
+      country: "GB",
+      city: "Ballymena",
+      count: 193033,
+      locations: 1,
+      firstUpdated: "2016-02-27T21:00:00+00:00",
+      lastUpdated: "2022-09-21T04:00:00+00:00",
+      parameters: ["no2", "so2"],
+    },
+    {
+      country: "GB",
+      city: "Barking and Dagenham",
+      count: 115662,
+      locations: 2,
+      firstUpdated: "2021-04-29T00:00:00+00:00",
+      lastUpdated: "2022-09-21T04:00:00+00:00",
+      parameters: ["no2", "pm10", "so2"],
+    },
+    {
+      country: "GB",
+      city: "Barnsley",
+      count: 362444,
+      locations: 1,
+      firstUpdated: "2016-02-27T21:00:00+00:00",
+      lastUpdated: "2022-09-21T05:00:00+00:00",
+      parameters: ["no2", "o3", "so2"],
+    },
+  ],
+};
 
 function App() {
+  const [currentCities, setCurrentCities] = useState([]);
+  const [selectedCity, setSeletedCity] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={results.results.map((option) => option.city)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+            }}
+          />
+        )}
+      />
     </div>
   );
 }
